681a5d661ab5c2653aa46699013ff34c2b2681e1
Lee DeBoom <lee@kicksite.net>
2018-10-11 16:11:32 -0500

    DEV-1729

--- lib/kicksite_auth_client.rb
+++ lib/kicksite_auth_client.rb
@@ -4,6 +4,6 @@ Dir["#{File.dirname(__FILE__)}/kicksite_auth_client/**/*.rb"].each { |file| requ
 module KicksiteAuthClient
   # Note: Anyone consuming this should mock the actual REST calls in their tests
   unless ENV['RAILS_ENV'] == 'test'
-    raise 'Set KICKSITE_AUTH_URL environment variable to utilize this gem' if ENV['KICKSITE_SVC_URL'].blank?
+    raise 'Set KICKSITE_AUTH_URL environment variable to utilize this gem' if ENV['KICKSITE_AUTH_URL'].blank?
   end
 end
