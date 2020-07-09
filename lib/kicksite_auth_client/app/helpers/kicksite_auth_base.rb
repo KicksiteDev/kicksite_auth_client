8d36f80caff4ce26a6078f26594460db43edec22
Lee DeBoom <lee@kicksite.net>
2020-07-09 15:46:19 -0500

    DEV-4425: Investigate and introduce methods of caching to help improve TTFB

--- lib/kicksite_auth_client/app/helpers/kicksite_auth_base.rb
+++ lib/kicksite_auth_client/app/helpers/kicksite_auth_base.rb
@@ -3,4 +3,19 @@ require 'activeresource'
 # Our base that primarily just defines the kicksite-auth base url
 class KicksiteAuthBase < ActiveResource::Base
   self.site = "#{ENV['KICKSITE_AUTH_URL']}/v1"
+
+  def to_hash
+    hash = {}
+    attributes.each do |key, value|
+      hash[key.to_s] = if value.present? && value.respond_to?(:to_hash)
+                         value.to_hash
+                       elsif value.present? && value.respond_to?(:attributes)
+                         value.attributes
+                       else
+                         value
+                       end
+    end
+
+    hash
+  end
 end
